177cf060f93f5f97d39d031f79692f8335fb2df0
Nick Berry <nicholasberry.13@gmail.com>
2017-01-08 19:00:55 -0500

    CRD?

---
+++ app/controllers/programs_controller.rb
@@ -0,0 +1,40 @@
+class ProgramsController < ApplicationController
+  def index
+    programs = Program.all
+    render json: programs, status: 200
+  end
+
+  def show
+    program = Program.find(program_params[:id])
+    render json: program, status: 200
+  end
+
+  def create
+    program = Program.create(program_params[:program])
+    render json: program, status: 201
+  end
+
+  def update
+    program = Program.find(program_params[:id])
+    program.update(program_params[:name, :description])
+    render json: program, status: 201
+  end
+
+  def destroy
+    program = Program.find(program_params[:id])
+    program.destroy
+    render 'Successfully deleted program.', status: 200
+  end
+
+  private
+
+  def program_params
+    params.permit(
+    :id,
+    :name,
+    :start_date,
+    :end_date,
+    :program => [:name, :id, :goal, :keywords, :duration, :start_date, :end_date]
+    )
+  end
+end
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,11 +1,17 @@
-class Post
+class Program
   include Mongoid::Document
   include Mongoid::Paperclip
+  include Mongoid::Timestamps
+
+  validates :name, presence: true
+  validates :duration, presence: true
+
   # validates :image, presence: true
 
   has_mongoid_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   field :name, type: String
-  field :description, type: String
-  field :keywords, type: Array
+  field :goal, type: String
+  field :tags, type: Array
+  field :duration, type: Integer
 end
